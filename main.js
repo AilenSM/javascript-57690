52831711a7dfd27ef4ba108b98bd9869e93f8e82
AilenSM <saucedoailen01@gmail.com>
2024-04-29 22:29:38 -0300

    1preentrega-function1

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 alert("Elige tu propia aventura: El abominable hombre de las nieves");
-let nombre = (prompt("¿Cómo deberia llamarse el protagonista de la aventura?")).toLocaleLowerCase()
+function nombreIngresado(){
+        let nombre = (prompt("¿Cómo deberia llamarse el protagonista de la aventura?")).toLocaleLowerCase()
+}
 let jugar = " Ingrese JUGAR para continuar";
 
-let advertencia = (prompt("Hola " + nombre +  " , este libro es distinto de otros, ¡Tu y solo TU eres el responsable de lo que ocurra en la historia!" + jugar));
+let advertencia = (prompt("Hola " + nombreIngresado() +  " , este libro es distinto de otros, ¡Tu y solo TU eres el responsable de lo que ocurra en la historia!" + jugar));
 
 if(advertencia == "jugar"){
-        alert(nombre + " hay peligros, disyuntivas, aventuras y consecuencias. Debes utilizar tus numerosos talentos y buena parte de tu enorme inteligencia. Una decision equivocada puede acabar en desastre, incluso en la muerte. No desesperes, en cualquier momento podes volver atras y elegir otra opcion, modificar el camino de tu historia y cambiar el resultado");
+        alert(nombreIngresado() + " hay peligros, disyuntivas, aventuras y consecuencias. Debes utilizar tus numerosos talentos y buena parte de tu enorme inteligencia. Una decision equivocada puede acabar en desastre, incluso en la muerte. No desesperes, en cualquier momento podes volver atras y elegir otra opcion, modificar el camino de tu historia y cambiar el resultado");
 }else(advertencia = "");{
         alert("sera la proxima!");
 }
